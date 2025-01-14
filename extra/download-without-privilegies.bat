bcc20e44fa96b623d8a0e888dcb2677369558b75
Cekkiq <157998362+Cekkiq@users.noreply.github.com>
2025-01-14 19:46:25 +0100

    Update download-without-privilegies.bat

--- extra/download-without-privilegies.bat
+++ extra/download-without-privilegies.bat
@@ -1,21 +1,2 @@
-@echo off
-setlocal enabledelayedexpansion
-
-if "%1"=="" (
-    echo Nebyl zadaný parametr. Použijte například: c.bat -f neco.bat
-    exit /b
-)
-
-if "%1"=="-f" (
-    if "%2"=="" (
-        echo Nebyl zadaný soubor pro -f.
-        exit /b
-    )
-    set param=%2
-    set __COMPAT_LAYER=RunAsInvoker
-    start "" "%param%"
-) else (
-    echo Neplatný parametr: %1
-    echo Použijte například: c.bat -f neco.bat
-    exit /b
-)
+set __COMPAT_LAYER=RunAsInvoker
+start <exe file> 
