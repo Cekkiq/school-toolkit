963d4b40cf8b04167062e85f394ddadf1ef3b3b2
Cekkiq <157998362+Cekkiq@users.noreply.github.com>
2025-01-14 19:43:54 +0100

    Update download-without-privilegies.bat

--- extra/download-without-privilegies.bat
+++ extra/download-without-privilegies.bat
@@ -1,2 +1,21 @@
-set __COMPAT_LAYER=RunAsInvoker
-start <name of exe in same folder>
+@echo off
+setlocal enabledelayedexpansion
+
+set targetFile=target.bat
+
+if "%1"=="" (
+    set param=default.bat
+) else (
+    set param=%*
+)
+
+(
+    for /f "tokens=*" %%a in ('findstr /v /b "set __COMPAT_LAYER" "%targetFile%"') do (
+        @echo %%a
+    )
+) > temp.bat
+
+echo set __COMPAT_LAYER=RunAsInvoker >> temp.bat
+echo start "%param%" >> neakysouborbezmezer.exe
+
+move /y temp.bat %targetFile% >nul
